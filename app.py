f0c3582a311cd2393e8b9c03819eb3098f8106e3
Victor Delgado <valexdelgado@gmail.com>
2022-12-02 12:29:53 -0800

    added model

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+app.app_context().push()
+
+migrate = Migrate(app, db)
+
+
+
+class Pet(db.Model):
+    """Pet."""
+
+    __tablename__ = "pets"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    name = db.Column(db.String(50),
+                     nullable=False,
+                     unique=True)
+    species = db.Column(db.String(30), nullable=True)
+    hunger = db.Column(db.Integer, nullable=False, default=20)
+
+    def greet(self):
+        """Greet using name."""
+
+        return f"I'm {self.name} the {self.species or 'thing'}"
+
+    def feed(self, units=10):
+        """Nom nom nom."""
+
+        self.hunger -= units
+        self.hunger = max(self.hunger, 0)
+
+    def __repr__(self):
+        """Show info about pet."""
+
+        p = self
+        return f"<Pet {p.id} {p.name} {p.species} {p.hunger}>"
+
+    @classmethod
+    def get_by_species(cls, species):
+        """Get all pets matching that species."""
+
+        return cls.query.filter_by(species=species).all()
